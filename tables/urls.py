5c1ef0f8f9acd1d970262d42b28b951607143e9a
Deathik <smertelny@gmail.com>
2018-01-24 16:35:30 +0200

    Added view for multiple teacher timetables

--- tables/urls.py
+++ tables/urls.py
@@ -6,4 +6,5 @@ app_name = 'timetable'
 urlpatterns = [
     path('', views.index, name='index'),
     path('all', views.all, name='all'),
+    path('test', views.test, name='test'),
 ]
--- tables/views.py
+++ tables/views.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.shortcuts import render
 
+from teachers.models import Teacher
 from .models import Timetable, DAY_OF_THE_WEEK
 
 def index(request):
@@ -14,4 +15,10 @@ def all(request):
     return render(request, 'timetable/all.html', {'all': all})
 
 def test(request):
-    return render(request, 'timetable/index.html', {})
+    teachers = Teacher.objects.all()
+    q = request.GET.get('q', None)
+    if q:
+        weekday = DAY_OF_THE_WEEK[datetime.date.today().weekday()][0]
+        lessons = Timetable.objects.filter(teacher__id=q).filter(weekday=weekday).order_by('lesson_number')
+        return render(request, 'timetable/test.html', {'teachers': teachers, 'lessons': lessons})
+    return render(request, 'timetable/test.html', {'teachers': teachers})
